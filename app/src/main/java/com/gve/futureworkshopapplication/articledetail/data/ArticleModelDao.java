cace4f389122e9760a51ead6c5ab6829e4be2d9b
valverde Guillaume <guillaume.valverde@gmail.com>
2017-11-09 11:04:40 +0100

    clean ArticleModelDAO

--- app/src/main/java/com/gve/futureworkshopapplication/articledetail/data/ArticleModelDao.java
+++ app/src/main/java/com/gve/futureworkshopapplication/articledetail/data/ArticleModelDao.java
@@ -1,14 +1,11 @@
 package com.gve.futureworkshopapplication.articledetail.data;
 
-import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
 import com.gve.futureworkshopapplication.articlelist.data.Article;
-
-import java.util.List;
 
 import io.reactivex.Flowable;
 import io.reactivex.Single;
@@ -22,9 +19,6 @@ import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
 @Dao
 public interface ArticleModelDao {
 
-    @Query("select * from articles")
-    Flowable<List<Article>> getAllBorrowedItems();
-
     @Query("select * from articles where id = :id")
     Flowable<Article> getItembyId(int id);
 
@@ -37,6 +31,4 @@ public interface ArticleModelDao {
     @Query("select * from articles where id = :id")
     Single<Article> getItembyIdSingle(int id);
 
-//    @Query("UPDATE article set article.favourite = :favourite from articles  WHERE id = :tid")
- //   int updateFavourite(long tid, Boolean favourite);
 }
